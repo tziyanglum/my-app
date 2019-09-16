1732a9a9c60463da734baaf1f88412eeb7f36922
tziyanglum <tziyanglum@outlook.com>
2019-09-16 20:28:44 +0200

    revert

--- src/app/components/container/skills/skills.component.ts
+++ src/app/components/container/skills/skills.component.ts
@@ -1,5 +1,17 @@
 import { Component } from "@angular/core";
 import { NgbRatingConfig } from "@ng-bootstrap/ng-bootstrap";
+import {
+  IconDefinition,
+  faAngular,
+  faHtml5,
+  faSass,
+  faJs,
+  faBootstrap,
+  faNodeJs,
+  faDocker,
+  faGithub,
+  faCss3Alt
+} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: "app-skills",
@@ -7,6 +19,16 @@ import { NgbRatingConfig } from "@ng-bootstrap/ng-bootstrap";
   styleUrls: ["./skills.component.scss"]
 })
 export class SkillsComponent {
+  public faAngular: IconDefinition = faAngular;
+  public faHtml5: IconDefinition = faHtml5;
+  public faSass: IconDefinition = faSass;
+  public faJs: IconDefinition = faJs;
+  public faBootstrap: IconDefinition = faBootstrap;
+  public faNodeJs: IconDefinition = faNodeJs;
+  public faGithub: IconDefinition = faGithub;
+  public faDocker: IconDefinition = faDocker;
+  public faCss3Alt: IconDefinition = faCss3Alt;
+
   constructor(config: NgbRatingConfig) {
     config.max = 5;
     config.readonly = true;
